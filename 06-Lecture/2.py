e78ed1f4fa93f43a60cda26b5e63c8945eb7f652
Attilio Di Vicino <attiliodivicino@libero.it>
2025-04-02 12:45:08 +0200

    t

--- 06-Lecture/2.py
+++ 06-Lecture/2.py
@@ -1,30 +1,3 @@
-# Create a DynamoDB table:
-dyndb=boto3.resource('dynamodb',region_name='us-west-2')
-
-table = dyndb.create_table(TableName='DataTable',
-	KeySchema =[
-		{'AttributeName':'PartitionKey','KeyType': 'HASH'},
-		{'AttributeName':'RowKey','KeyType':'RANGE'}
-	],
-	AttributeDefinitions=[
-		{'AttributeName':'PartitionKey','AttributeType':'S'},
-		{'AttributeName':'RowKey','AttributeType':'S'}
-	],
-	ProvisionedThroughput=
-		{'ReadCapacityUnits':1,'WriteCapacityUnits':1}
-)
-table.meta.client.get_waiter('table_exists').wait(TableName='DataTable')
-
-# Upload a file:
-s3 = boto3.resource('s3')
-s3.Object('datacont', 'test.jpg' ).put(
-	Body=open( '/home/mydata/test.jpg' , 'rb')
-)
-
-# Using an already defined DynamoDB table:
-dyndb=boto3.resource('dynamodb',region_name='us-west-2')
-table = dyndb.Table("DataTable")
-
 # Upload data to the table:
 import csv
 dyndb=boto3.resource('dynamodb',region_name='us-west-2')
