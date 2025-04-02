f99c4a6eff7f92e547fdf57ddf75b45cb2e86b84
Attilio Di Vicino <attiliodivicino@libero.it>
2025-04-02 12:41:14 +0200

    t

--- 06-Lecture/upload_from.py
+++ 06-Lecture/upload_from.py
@@ -4,13 +4,13 @@ dyndb=boto3.resource('dynamodb',region_name='us-west-2')
 
 table = dyndb.Table("DataTable")
 urlbase = "https://s3-us-west-2.amazonaws.com/it-attiliodivicino-datacont/"
-with open( 'https://it-attiliodivicino-datacont.s3.us-west-2.amazonaws.com/experiments.csv' , ' rb') as csvfile:
+with open( 'https://it-attiliodivicino-datacont.s3.us-west-2.amazonaws.com/experiments.csv' , 'rb') as csvfile:
 	csvf = csv.reader(csvfile,
 		delimiter =',',
 		quotechar='|'
 	)
 	for item in csvf:
-		body=open('https://it-attiliodivicino-datacont.s3.us-west-2.amazonaws.com/\\'+item[3],'rb ')
+		body=open('https://it-attiliodivicino-datacont.s3.us-west-2.amazonaws.com/\\'+item[3],'rb')
 		s3.Object('datacont',item[3]).put(Body=body)
 		md=s3.Object('datacont',item[3]).Acl().put(ACL='public-read')
 		url= urlbase +item [3]
