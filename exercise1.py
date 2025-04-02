3ecd0239221ce4ba9e10acda2af6ebda9fab1f65
Attilio Di Vicino <attiliodivicino@libero.it>
2025-04-02 12:05:08 +0200

    first ecercise

--- exercise1.py
+++ exercise1.py
@@ -2,7 +2,6 @@ import boto3
 s3 = boto3.resource('s3')
 
 # Bucket creation:
-import boto3
 s3 = boto3.resource('s3')
 s3.create_bucket(
 	Bucket = 'datacont',
@@ -12,7 +11,6 @@ s3.create_bucket(
 )
 
 # Create a DynamoDB table:
-import boto3
 dyndb=boto3.resource('dynamodb',region_name='us-west-2')
 
 table = dyndb.create_table(TableName='DataTable',
@@ -27,49 +25,38 @@ table = dyndb.create_table(TableName='DataTable',
 	ProvisionedThroughput=
 		{'ReadCapacityUnits':1,'WriteCapacityUnits':1}
 )
-	table.meta.client.get_waiter('table_exists').
-		wait(TableName='DataTable')
+table.meta.client.get_waiter('table_exists').wait(TableName='DataTable')
 
-# Upload a file:
-import boto3
+# Upload a file:
 s3 = boto3.resource('s3')
 s3.Object('datacont', 'test.jpg' ).put(
-	Body=opimport boto3
-s3 = boto3.resource('s3')
-
-# Bucket creation:
-import boto3
-s3 = boto3.resource('s3')
-s3.create_bucket(
-        Bucket = 'datacont',
-        CreateBucketConfiguration={
-                'LocationConstraint': 'us-west-2'
-        }
+	Body=open( '/home/mydata/test.jpg' , 'rb')
 )
 
-# Create a DynamoDB table:
-import boto3
+# Using an already defined DynamoDB table:
+dyndb=boto3.resource('dynamodb',region_name='us-west-2')
+table = dyndb.Table("DataTable")
+
+# Upload data to the table:
+import csv
 dyndb=boto3.resource('dynamodb',region_name='us-west-2')
 
-table = dyndb.create_table(TableName='DataTable',
-        KeySchema =[
-                {'AttributeName':'PartitionKey','KeyType': 'HASH'},
-                {'AttributeName':'RowKey','KeyType':'RANGE'}
-        ],
-        AttributeDefinitions=[
-                {'AttributeName':'PartitionKey','AttributeType':'S'},
-                {'AttributeName':'RowKey','AttributeType':'S'}
-        ],
-        ProvisionedThroughput=
-                {'ReadCapacityUnits':1,'WriteCapacityUnits':1}
-)
-        table.meta.client.get_waiter('table_exists').
-                wait(TableName='DataTable')
-
-# Upload a file:
-import boto3
-s3 = boto3.resource('s3')
-s3.Object('datacont', 'test.jpg' ).put(
-        Body=open( '/home/mydata/test.jpg' , 'rb')
-n( '/home/mydata/test.jpg' , 'rb')
-)
+table = dyndb.Table("DataTable")
+urlbase = "https://s3-us-west-2.amazonaws.com/datacont/"
+with open( '\path-to-your-data\experiments.csv' , ' rb') as csvfile:
+	csvf = csv.reader(csvfile,
+		delimiter =',',
+		quotechar='|'
+	)
+	for item in csvf:
+		body=open('path-to-your-data\datafiles\\'+item[3],'rb ')
+		s3.Object('datacont',item[3]).put(Body=body)
+		md=s3.Object('datacont',item[3]).Acl().put(ACL='public-read')
+		url= urlbase +item [3]
+		metadata_item={
+			'PartitionKey':item[0],
+			'RowKey':item[1],
+			'Description':item[4],
+			'Date':item[2],
+			'Url':url}
+		table.put_item(Item= metadata_item)
